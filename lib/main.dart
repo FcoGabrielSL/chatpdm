30ac56b8fd51c542c956160f76a84113a7816c20
FcoGabrielsl <fcogabriel373@gmail.com>
2020-10-09 15:12:07 -0300

    g

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:chatpdm/helpers/firebase_helper.dart';
+import 'package:chatpdm/ui/chat_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  //iniciar o FireBase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  //FireBaseHelper helper = FireBaseHelper();
+  //helper.sendMenssage("text");
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat app',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          iconTheme: IconThemeData(color: Colors.blue)),
+      home: ChatPage(),
+    );
+  }
+}
